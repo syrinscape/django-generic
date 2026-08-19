ba1158060b7fd22d82ad03b78a8b07e75115f8d6
Tai Lee <real.human@mrmachine.net>
2026-08-20 01:30:26 +1000

    fix(models): follow Django 1.8 reverse relations
    Django 1.8 exposes the child class of a reverse parent link as
    related_model while model can identify the source class. Using model
    therefore skips valid leaves and catches the wrong DoesNotExist
    exception.
    
    Resolve and reuse related_model for traversal and exception handling,
    falling back to model for Django 1.7 relation objects that do not expose
    the newer attribute.

--- generic/mixins.py
+++ generic/mixins.py
@@ -5,10 +5,11 @@ class Inheritable(object):
         """Returns the model instance object as instance of the outermost leaf class,
         by searching through its related descriptors to find the right model."""
         for obj in self._meta.get_all_related_objects():
-            if obj.model != self.__class__ and obj.field.rel.parent_link:
+            related_model = getattr(obj, 'related_model', obj.model)
+            if related_model != self.__class__ and obj.field.rel.parent_link:
                 try:
                     return getattr(self, obj.get_accessor_name()).get_leaf_object()
-                except obj.model.DoesNotExist:
+                except related_model.DoesNotExist:
                     pass
         return self
 
--- generic/tests.py
+++ generic/tests.py
@@ -9,6 +9,7 @@ from django.test.client import RequestFactory
 from . import decorators
 from .admin.mixins.cooking import BaseCookedIdAdmin, CookedSingletonFix
 from .admin.mixins.polymorphic import PolymorphicAdmin
+from .mixins import Inheritable
 
 request_factory = RequestFactory()
 urlpatterns = patterns('', url(r'^admin/', include(admin.site.urls)))
@@ -86,3 +87,92 @@ class CookedIdAdminTests(unittest.TestCase):
         model_admin = CookedGroupAdmin(Group, admin.site)
 
         self.assertIn('/admin/auth/group/add/', unicode(model_admin.media))
+
+
+class InheritableTests(unittest.TestCase):
+    class RelationField(object):
+        class rel(object):
+            parent_link = True
+
+    class Meta(object):
+        def __init__(self, related_objects):
+            self.related_objects = related_objects
+
+        def get_all_related_objects(self):
+            return self.related_objects
+
+    class Relation(object):
+        def __init__(self, model, related_model, accessor_name):
+            self.model = model
+            self.related_model = related_model
+            self.field = InheritableTests.RelationField()
+            self.accessor_name = accessor_name
+
+        def get_accessor_name(self):
+            return self.accessor_name
+
+    class LegacyRelation(object):
+        def __init__(self, model, accessor_name):
+            self.model = model
+            self.field = InheritableTests.RelationField()
+            self.accessor_name = accessor_name
+
+        def get_accessor_name(self):
+            return self.accessor_name
+
+    def test_get_leaf_object_follows_parent_link_to_related_model(self):
+        class Parent(Inheritable):
+            class DoesNotExist(Exception):
+                pass
+
+        class Leaf(Inheritable):
+            class DoesNotExist(Exception):
+                pass
+
+        relation = self.Relation(Parent, Leaf, 'leaf')
+        parent = Parent()
+        leaf = Leaf()
+        parent._meta = self.Meta([relation])
+        parent.leaf = leaf
+        leaf._meta = self.Meta([])
+
+        self.assertIs(parent.get_leaf_object(), leaf)
+
+    def test_get_leaf_object_ignores_absent_related_model(self):
+        class Parent(Inheritable):
+            class DoesNotExist(Exception):
+                pass
+
+        class Leaf(Inheritable):
+            class DoesNotExist(Exception):
+                pass
+
+        class MissingLeaf(object):
+            def __get__(self, instance, owner):
+                instance.lookup_count += 1
+                raise Leaf.DoesNotExist
+
+        Parent.leaf = MissingLeaf()
+        relation = self.Relation(Parent, Leaf, 'leaf')
+        parent = Parent()
+        parent._meta = self.Meta([relation])
+        parent.lookup_count = 0
+
+        self.assertIs(parent.get_leaf_object(), parent)
+        self.assertEqual(parent.lookup_count, 1)
+
+    def test_get_leaf_object_falls_back_to_relation_model(self):
+        class Parent(Inheritable):
+            pass
+
+        class Leaf(Inheritable):
+            pass
+
+        relation = self.LegacyRelation(Leaf, 'leaf')
+        parent = Parent()
+        leaf = Leaf()
+        parent._meta = self.Meta([relation])
+        parent.leaf = leaf
+        leaf._meta = self.Meta([])
+
+        self.assertIs(parent.get_leaf_object(), leaf)
